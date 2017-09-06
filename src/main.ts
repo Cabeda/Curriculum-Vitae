a94a654564d6781e677956819c6ae4018b45de8b
José Cabeda <1130395@isep.ipp.pt>
2017-09-06 23:44:21 +0100

    Removed hmr dependency

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,6 @@ import { enableProdMode } from '@angular/core';
 import { environment } from './environments/environment';
 import { AppModule } from './app/app.module';
 
-import { hmrBootstrap } from './hmr';
-
 if (environment.production) {
   enableProdMode();
 }
@@ -14,13 +12,5 @@ const bootstrap = () => {
   return platformBrowserDynamic().bootstrapModule(AppModule);
 };
 
-if (environment.hmr) {
-  if (module[ 'hot' ]) {
-    hmrBootstrap(module, bootstrap);
-  } else {
-    console.error('HMR is not enabled for webpack-dev-server!');
-    console.log('Are you using the --hmr flag for ng serve?');
-  }
-} else {
   bootstrap();
-}
+
