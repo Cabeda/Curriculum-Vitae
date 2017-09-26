d1cdac6089807102167e02fccdaf62394141a0ad
José Cabeda <jecabeda@gmail.com>
2017-09-27 00:53:13 +0100

    Improved the main page

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,20 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { environment } from './environments/environment';
 import { AppModule } from './app/app.module';
+import * as jquery from 'jquery';
+window['jQuery'] = window['$'] = jquery;
+
+import * as tether from 'tether';
+window['Tether'] = tether;
 
 if (environment.production) {
   enableProdMode();
 }
 
+
 const bootstrap = () => {
   return platformBrowserDynamic().bootstrapModule(AppModule);
 };
 
-  bootstrap();
+jquery(() => bootstrap());
 
