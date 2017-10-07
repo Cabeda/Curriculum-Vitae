e27dcd786dcc50e23ffe583a07af10b0b75f792d
José Cabeda <jecabeda@gmail.com>
2017-10-07 02:32:57 +0100

    Removed unnecessary scripts

--- scripts/remove-branch.sh
+++
@@ -1,5 +0,0 @@
-set -e
-git ls-remote --heads | grep gh-pages > /dev/null
-if [ "$?" == "0" ]; then
-  git push origin --delete gh-pages
-fi
--- scripts/setup-github-acess.sh
+++
@@ -1,4 +0,0 @@
-set -e
-echo "machine github.com" >> ~/.netrc
-echo "   login jecabeda@gmail.com" >> ~/.netrc
-echo "   password ${GH_TOKEN}" >> ~/.netrc
